414628c0a5bb3c8dd9163890b452e8a5734d2a17
kirill <kirisa49@gmail.com>
2025-09-16 17:03:19 +0700

    fix env.py

--- app/env.py
+++ app/env.py
@@ -1,12 +1,12 @@
 from logging.config import fileConfig
 from sqlalchemy import pool
 from sqlalchemy.ext.asyncio import create_async_engine
-
 from alembic import context
-
 from app.db.base import Base
 from config import settings
+import asyncio
 
+# Alembic config
 config = context.config
 fileConfig(config.config_file_name)
 target_metadata = Base.metadata
@@ -14,23 +14,24 @@ target_metadata = Base.metadata
 DATABASE_URL = settings.DATABASE_URL
 
 def run_migrations_offline():
-    url = DATABASE_URL
-    context.configure(url=url, target_metadata=target_metadata, literal_binds=True)
+    context.configure(
+        url=DATABASE_URL,
+        target_metadata=target_metadata,
+        literal_binds=True,
+    )
     with context.begin_transaction():
         context.run_migrations()
 
-def run_migrations_online():
+def do_run_migrations_online():
     connectable = create_async_engine(DATABASE_URL, poolclass=pool.NullPool)
 
-    async def do_run_migrations():
+    async def run():
         async with connectable.begin() as conn:
             await conn.run_sync(lambda sync_conn: context.configure(connection=sync_conn, target_metadata=target_metadata))
             await conn.run_sync(lambda sync_conn: context.run_migrations())
-
-    import asyncio
-    asyncio.run(do_run_migrations())
+    asyncio.run(run())
 
 if context.is_offline_mode():
     run_migrations_offline()
 else:
-    run_migrations_online()
+    do_run_migrations_online()
